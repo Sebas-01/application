e27f52a1fadc2909d78bfb81e558d7cbba7592a6
Sebas-01 <128066214+Sebas-01@users.noreply.github.com>
2024-02-20 21:27:44 -0500

    Add files via upload

---
+++ Myaplicacion_sebas/app/src/main/java/com/example/myaplicacion_sebas/MainActivity.java
@@ -0,0 +1,99 @@
+package com.example.myaplicacion_sebas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView nombreUsuario;
+    Button btn1,btn2,send;
+    String Tag = "Prueba";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        nombreUsuario = (TextView) findViewById(R.id.usuario);
+        btn1 = findViewById(R.id.btn1);
+        btn2 = findViewById(R.id.btn2);
+        send = findViewById(R.id.btnSend);
+
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                nombreUsuario.setText("Presioné Boton Uno");
+
+            }
+        });
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                nombreUsuario.setText("Presioné Boton Dos");
+            }
+        });
+
+        send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                enviarPantalla2();
+            }
+        });
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(Tag,"Estoy en OnStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(Tag,"Estoy en OnResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(Tag,"Estoy en OnPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(Tag,"Estoy en OnStop");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(Tag,"Estoy en OnRestart");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(Tag,"Estoy en OnDestroy");
+    }
+
+    privated void enviarPantalla2(){
+        String textoActual = nombreUsuario.getText().toString();
+        String nombre = "sebastian"
+        intent pasarinfo = new Intent(this,Pantalla2.class)
+        pasarinfo.putExtra("name", nombre);
+        starActivity(pasarinfo);
+    }
+
+}
+
+
